a93a1f1088e1781f36006672d640708ad116ad9f
AMattiza <andreas@choose-hub.com>
2025-05-15 09:50:32 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ export default function App() {
     salesCost: 1.5,
     logisticsCost: 0.5,
     unitsPerDisplay: 32,
-    newPartners: 6,
+    newPartners: 5,
     increaseInterval: 3,
     increaseAmount: 1,
     reorderRate: 75,
